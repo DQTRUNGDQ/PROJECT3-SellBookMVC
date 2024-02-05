b1b8c6b7d64fe8256323d6f33ff30430308737fc
DQTRUNGDQ <trungthpthy@gmail.com>
2024-02-05 22:13:12 +0700

    started

---
+++ SellBookWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SellBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
